332c80b254522277df831c55bcd48b76bb42156b
Parsa <142115135+ParsaKSH@users.noreply.github.com>
2025-01-28 23:09:13 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -52,7 +52,6 @@ function ask_yes_no() {
 echo -e "\033[1;33mInstalling WireGuard (if not installed)...\033[0m"
 sudo apt update
 sudo apt install wireguard -y
-sudo rm /etc/wireguard/wg86.conf 2>/dev/null
 
 read -p "Are you running this script on the IRAN server or the FOREIGN server? (IRAN/FOREIGN): " server_location
 
@@ -94,8 +93,8 @@ if [[ "$server_location" == "IRAN" || "$server_location" == "iran" ]]; then
     read -p "Please enter the MTU (default 1380): " mtu
     mtu=${mtu:-1380}
 
-    echo -e "\033[1;33mCreating /etc/wireguard/TAQ-BOSTAN-wg.conf for IRAN server...\033[0m"
-    sudo bash -c "cat > /etc/wireguard/TAQ-BOSTAN-wg.conf <<EOF
+    echo -e "\033[1;33mCreating /etc/wireguard/wg86.conf for IRAN server...\033[0m"
+    sudo bash -c "cat > /etc/wireguard/wg86.conf <<EOF
 [Interface]
 PrivateKey = $IRAN_PRIV_KEY
 MTU = $mtu
@@ -105,7 +104,7 @@ EOF"
 
     count_even=2
     for (( i=1; i<=$n_foreign; i++ )); do
-        sudo bash -c "cat >> /etc/wireguard/TAQ-BOSTAN-wg <<EOF
+        sudo bash -c "cat >> /etc/wireguard/wg86.conf <<EOF
 
 [Peer]
 PublicKey = ${foreign_pubs[i]}
@@ -116,11 +115,10 @@ EOF"
         count_even=$((count_even + 2))
     done
 
-    sudo chmod 600 /etc/wireguard/TAQ-BOSTAN-wg.conf
+    sudo chmod 600 /etc/wireguard/wg86.conf
     sudo wg-quick down wg86 2>/dev/null
-    sudo wg-quick down TAQ-BOSTAN-wg 2>/dev/null
-    sudo wg-quick up TAQ-BOSTAN-wg
-    sudo systemctl enable wg-quick@TAQ-BOSTAN-wg
+    sudo wg-quick up wg86
+    sudo systemctl enable wg-quick@wg86
 
     echo -e "\033[1;32mWireGuard configuration for IRAN server is ready.\033[0m"
     echo -e "IRAN server public key: \033[1;37m${IRAN_PUB_KEY}\033[0m"
@@ -165,8 +163,8 @@ else
     read -p "Please enter the MTU (default 1380): " mtu
     mtu=${mtu:-1380}
 
-    echo -e "\033[1;33mCreating /etc/wireguard/TAQ-BOSTAN-wg.conf for FOREIGN server...\033[0m"
-    sudo bash -c "cat > /etc/wireguard/TAQ-BOSTAN-wg.conf <<EOF
+    echo -e "\033[1;33mCreating /etc/wireguard/wg86.conf for FOREIGN server...\033[0m"
+    sudo bash -c "cat > /etc/wireguard/wg86.conf <<EOF
 [Interface]
 PrivateKey = $FOR_PRIV_KEY
 MTU = $mtu
@@ -180,10 +178,10 @@ Endpoint = $iran_ip:$wg_port
 PersistentKeepalive = 20
 EOF"
 
-    sudo chmod 600 /etc/wireguard/TAQ-BOSTAN-wg.conf
+    sudo chmod 600 /etc/wireguard/wg86.conf
     sudo wg-quick down wg86 2>/dev/null
-    sudo wg-quick up TAQ-BOSTAN-wg
-    sudo systemctl enable wg-quick@TAQ-BOSTAN-wg
+    sudo wg-quick up wg86
+    sudo systemctl enable wg-quick@wg86
 
     echo -e "\033[1;32mWireGuard setup on FOREIGN server completed.\033[0m"
     echo -e "FOREIGN server public key: \033[1;37m${FOR_PUB_KEY}\033[0m"
