edc0751673c8f47b20d96cb38449db5578ac7596
Parsa <142115135+ParsaKSH@users.noreply.github.com>
2025-03-11 06:04:02 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -111,7 +111,7 @@ EOF"
 PublicKey = ${foreign_pubs[i]}
 AllowedIPs = 2619:db8:85a3:1b2::${count_even}/128
 Endpoint = ${foreign_ips[i]}:$wg_port
-PersistentKeepalive = 20
+PersistentKeepalive = 15
 EOF"
         count_even=$((count_even + 2))
     done
@@ -176,7 +176,7 @@ ListenPort = $wg_port
 PublicKey = $iran_pub
 AllowedIPs = 2619:db8:85a3:1b2::1/128
 Endpoint = $iran_ip:$wg_port
-PersistentKeepalive = 20
+PersistentKeepalive = 15
 EOF"
 
     sudo chmod 600 /etc/wireguard/TAQBOSTANwg.conf
