45e8268fd51fff0a380d4488dd2fe7aa1f181540
Parsa <142115135+ParsaKSH@users.noreply.github.com>
2025-03-15 21:33:20 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,3 @@
-
-
 GREEN="\e[32m"
 BOLD_GREEN="\e[1;32m"
 YELLOW="\e[33m"
@@ -14,36 +12,6 @@ draw_green_line() {
   echo -e "${GREEN}+--------------------------------------------------------+${RESET}"
 }
 
-print_art() {
-  echo -e "\033[1;32m                                                           "
-  echo -e "@@@@@@@   @@@@@@    @@@@@@                                 "
-  echo -e "@@@@@@@  @@@@@@@@  @@@@@@@@                                "
-  echo -e "  @@!    @@!  @@@  @@!  @@@                                "
-  echo -e "  !@!    !@!  @!@  !@!  @!@                                "
-  echo -e "  @!!    @!@!@!@!  @!@  !@!                                "
-  echo -e "  !!!    !!!@!!!!  !@!  !!!                                "
-  echo -e "  !!:    !!:  !!!  !!:!!:!:                                "
-  echo -e "  :!:    :!:  !:!  :!: :!:                                 "
-  echo -e "   ::    ::   :::  ::::: :!                                "
-  echo -e "   :      :   : :   : :  :::                               "
-  echo -e "@@@@@@@    @@@@@@    @@@@@@  @@@@@@@   @@@@@@   @@@  @@@   "
-  echo -e "@@@@@@@@  @@@@@@@@  @@@@@@@  @@@@@@@  @@@@@@@@  @@@@ @@@   "
-  echo -e "@@!  @@@  @@!  @@@  !@@        @@!    @@!  @@@  @@!@!@@@   "
-  echo -e "!@   @!@  !@!  @!@  !@!        !@!    !@!  @!@  !@!!@!@!   "
-  echo -e "@!@!@!@   @!@  !@!  !!@@!!     @!!    @!@!@!@!  @!@ !!@!   "
-  echo -e "!!!@!!!!  !@!  !!!   !!@!!!    !!!    !!!@!!!!  !@!  !!!   "
-  echo -e "!!:  !!!  !!:  !!!       !:!   !!:    !!:  !!!  !!:  !!!   "
-  echo -e ":!:  !:!  :!:  !:!      !:!    :!:    :!:  !:!  :!:  !:!   "
-  echo -e " :: ::::  ::::: ::  :::: ::     ::    ::   :::   ::   ::   "
-  echo -e ":: : ::    : :  :   :: : :      :      :   : :  ::    :    "
-  echo -e "                                                           \033[0m"
-
-  echo -e "\033[1;33m=========================================================="
-  echo -e "Created by Parsa in OPIran club https://t.me/OPIranClub"
-  echo -e "Love Iran :)"
-  echo -e "\033[0m"
-}
-
 print_menu() {
   draw_green_line
   echo -e "${GREEN}|${RESET}              ${BOLD_GREEN}TAQ-BOSTAN Main Menu${RESET}                 ${GREEN}|${RESET}"
@@ -51,6 +19,10 @@ print_menu() {
   echo -e "${GREEN}|${RESET} ${BLUE}1)${RESET} Create best and safest tunnel                  ${GREEN}|${RESET}"
   echo -e "${GREEN}|${RESET} ${YELLOW}2)${RESET} Create local IPv6 with Sit                      ${GREEN}|${RESET}"
   echo -e "${GREEN}|${RESET} ${MAGENTA}3)${RESET} Create local IPv6 with Wireguard                ${GREEN}|${RESET}"
+  draw_green_line
+  echo -e "${GREEN}|${RESET} ${BLUE}4)${RESET} Delete tunnel                                  ${GREEN}|${RESET}"
+  echo -e "${GREEN}|${RESET} ${YELLOW}5)${RESET} Delete local IPv6 with Sit                      ${GREEN}|${RESET}"
+  echo -e "${GREEN}|${RESET} ${MAGENTA}6)${RESET} Delete local IPv6 with Wireguard                ${GREEN}|${RESET}"
   draw_green_line
 }
 
@@ -69,6 +41,32 @@ execute_option() {
       echo -e "${CYAN}Executing: Create local IPv6 with Wireguard...${RESET}"
       bash <(curl -Ls https://raw.githubusercontent.com/ParsaKSH/TAQ-BOSTAN/main/wireguard.sh)
       ;;
+    4)
+      echo -e "${CYAN}Deleting Hysteria tunnel...${RESET}"
+      sudo systemctl daemon-reload 2>/dev/null
+      for i in {,1,2,3,4,5,6,7,8}; do
+        sudo systemctl disable hysteria$i 2>/dev/null
+      done
+      sudo rm /etc/hysteria/server-config.yaml /etc/hysteria/iran-config{1..8}.yaml 2>/dev/null
+      echo -e "${GREEN}Hysteria tunnel successfully deleted.${RESET}"
+      ;;
+    5)
+      echo -e "${CYAN}Deleting local IPv6 with Sit...${RESET}"
+      for i in {,1,2,3,4,5,6,7,8}; do
+        sudo rm /etc/netplan/pdtun$i.yaml 2>/dev/null
+        sudo rm /etc/systemd/network/tun$i.network 2>/dev/null
+      done
+      sudo netplan apply 
+      sudo systemctl restart systemd-networkd
+      echo -e "${GREEN}Local IPv6 with Sit successfully deleted.${RESET}"
+      ;;
+    6)
+      echo -e "${CYAN}Deleting local IPv6 with Wireguard...${RESET}"
+      sudo wg-quick down TAQBOSTANwg 2>/dev/null
+      sudo systemctl disable wg-quick@TAQBOSTANwg 2>/dev/null
+      sudo rm /etc/wireguard/TAQBOSTANwg.conf 2>/dev/null
+      echo -e "${GREEN}Local IPv6 with Wireguard successfully deleted.${RESET}"
+      ;;
     *)
       echo -e "${RED}Invalid option. Exiting...${RESET}"
       exit 1
@@ -76,7 +74,6 @@ execute_option() {
   esac
 }
 
-print_art
 print_menu
-read -p "$(echo -e "${WHITE}Select an option [1-3]: ${RESET}")" user_choice
+read -p "$(echo -e "${WHITE}Select an option [1-6]: ${RESET}")" user_choice
 execute_option "$user_choice"
