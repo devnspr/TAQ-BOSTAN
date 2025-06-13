2fcc6138a507302b8721d36bbe26e36b734fec0c
Mohammad N. Dizgovin <naserpour1@gmail.com>
2025-06-13 13:00:36 +0200

    fix

--- script.sh
+++ script.sh
@@ -62,15 +62,14 @@ execute_option() {
   case "$choice" in
     1)
       echo -e "${CYAN}Executing: Create best and safest tunnel...${RESET}"
-      bash <(curl -Ls https://raw.githubusercontent.com/ParsaKSH/TAQ-BOSTAN/main/hysteria.sh)
-      ;;
+      bash ./hysteria.sh
     2)
       echo -e "${CYAN}Executing: Create local IPv6 with Sit...${RESET}"
-      bash <(curl -Ls https://raw.githubusercontent.com/ParsaKSH/TAQ-BOSTAN/main/sit.sh)
+      bash ./sit.sh
       ;;
     3)
       echo -e "${CYAN}Executing: Create local IPv6 with Wireguard...${RESET}"
-      bash <(curl -Ls https://raw.githubusercontent.com/ParsaKSH/TAQ-BOSTAN/main/wireguard.sh)
+      bash ./wireguard.sh
       ;;
     4)
       echo -e "${CYAN}Deleting Hysteria tunnel...${RESET}"
